ca94f309a81b6fc61a405ed98152ec3ec3fee093
Jeroen Pot <info@jeroenpot.nl>
2015-09-17 13:46:30 +0200

    Initial commit.

---
+++ Source/SimpleInjector.AssemblyScanner.UnitTests/TestClasses.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleInjector.AssemblyScanner.UnitTests
+{
+    public class AClass : IAClass
+    {
+    }
+
+    public interface IAClass
+    {
+    }
+
+    public class B1Class : IBClass
+    {
+    }
+
+    public class B2Class : IBClass
+    {
+    }
+
+    public interface IBClass
+    {
+    }
+
+    public class CDCLass : ICClass, IDClass
+    {
+    }
+
+    public interface ICClass
+    {
+    }
+
+    public interface IDClass
+    {
+    }
+
+    public class DontRegisterMe : IDontRegisterMe
+    {
+    }
+
+    public interface IDontRegisterMe
+    {
+    }
+
+    public class FirstImplementation : IHasMultipleImplementations
+    {
+    }
+
+    public class SecondImplementation : IHasMultipleImplementations
+    {
+    }
+
+    public interface IHasMultipleImplementations
+    {
+    }
+}
---
+++ Source/SimpleInjector.AssemblyScanner/DependencyConfigurationException.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SimpleInjector.AssemblyScanner
+{
+    /// <summary>
+    /// Exception that is thrown when registration fails.
+    /// </summary>
+    [Serializable]
+    public class DependencyConfigurationException : Exception
+    {
+        /// <summary>
+        /// Gets the validation errors.
+        /// </summary>
+        /// <value>
+        /// The validation errors.
+        /// </value>
+        public IList<string> ValidationErrors { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyConfigurationException"/> class.
+        /// </summary>
+        public DependencyConfigurationException()
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyConfigurationException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public DependencyConfigurationException(string message) : base(message)
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyConfigurationException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="validationErrors">The validation errors.</param>
+        public DependencyConfigurationException(string message, IList<string> validationErrors)
+            : base(message)
+        {
+            ValidationErrors = validationErrors;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyConfigurationException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public DependencyConfigurationException(string message, Exception inner) : base(message, inner)
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyConfigurationException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
+        protected DependencyConfigurationException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, sets the <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with information about the exception.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown. </param><param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination. </param><exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is a null reference (Nothing in Visual Basic). </exception>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("ValidationErrors", ValidationErrors);
+        }
+    }
+}
